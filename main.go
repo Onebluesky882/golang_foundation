5c71aa1464b2d1c203a16a76b0c371735ab546dc
Wansing <119560471+Onebluesky882@users.noreply.github.com>
2024-01-09 22:22:11 +0700

    day 3

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type KG float64
 type LB float64
+type ST string
 
 func main() {
-	k := KG(3)
-	l := LB(3)
-	fmt.Println(k == 3)
-	fmt.Println(l == 3)
+	k := KG(5.9)
+	b := LB(2.20462)
+
+	fmt.Println(k == b.toKG())
+	fmt.Println(k.toLB())
+	fmt.Println(k.toString())
+}
+
+func (lb LB) toKG() KG {
+
+	return KG(lb / 2.20462)
+}
+
+func (kg KG) toLB() LB {
+	return LB(kg / 0.453592)
+}
+
+func (kg KG) toString() ST {
+	return ST(strconv.Itoa(int(kg)) + " kg")
 }
