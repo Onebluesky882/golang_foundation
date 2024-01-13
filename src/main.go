69673cfc6d5b54730b83ddfd4f9a734cb86ea83c
Wansing <119560471+Onebluesky882@users.noreply.github.com>
2024-01-13 22:20:27 +0700

    day6

--- src/main.go
+++ src/main.go
@@ -1,9 +1,17 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+//Type struct
+type Person struct {
+	name string
+	age  int
+}
 
 func main() {
+	// แบบที่ 1
+	staff := Person{name: "wansing", age: 20}
+
+	fmt.Println(staff)
 
 }
