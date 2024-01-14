b25087026c121633a0796ae6fc06aa63ca2366f8
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-01-14 22:05:35 +0700

    day7

--- jsondemo/jsondemo.go
+++ jsondemo/jsondemo.go
@@ -1,2 +1,8 @@
 package jsondemo
 
+type Todo struct {
+	UserId    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title,omitempty"`
+	Completed *bool  `json:"completed,omitempty"`
+}
