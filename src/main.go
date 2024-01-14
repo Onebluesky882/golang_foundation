5d41e436f1c510ee478fee8089aa028b0a6f791e
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-01-14 16:40:47 +0700

    day7

--- src/main.go
+++ src/main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
-type BinaryTree struct {
-	value int
-	left  *BinaryTree
-	right *BinaryTree
-}
+import ()
 
 func main() {
-	root := BinaryTree{value: 2}
-	left := BinaryTree{value: 1}
-	right := BinaryTree{value: 3}
-
-	root.left = &left
-	root.right = &right
-
-	showDF(&root)
-}
-
-func showDF(node *BinaryTree) {
-
-	if node != nil {
-		showDF(node.left)
-		fmt.Println(node.value)
-		showDF(node.right)
-	}
 
 }
