aac316bea57f71e8c82ac1c0f9a3aafb365bc4f9
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-01-17 20:41:14 +0700

    day8

--- src/main.go
+++ src/main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"io/ioutil"
+	"text/template"
+
 	"jsondemo"
-	"net/http"
 	"os"
 )
 
 func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	// ## read from get http
+	// resp, err := http.Get("https://jsonplaceholder.typicode.com/todos")
+	// if err != nil {
+	// 	return
+	// }
+
+	// ## read from local file
+	resp, err := ioutil.ReadFile("/Users/onebluesky882/local_files/Learning/golearn/golang_foundation/src/todo.html")
 	if err != nil {
 		return
 	}
 
-	todoDecoder := json.NewDecoder(resp.Body)
+	todoDecoder := json.NewDecoder(bytes.NewReader(resp))
 	todos := jsondemo.Todo{}
 	todoDecoder.Decode(&todos)
 
-	todoEncoder := json.NewEncoder(os.Stdout)
-	todoEncoder.Encode(todos)
-
+	// indexTemplateBytes, err := ioutil.ReadFile("/Users/onebluesky882/local_files/Learning/golearn/golang_foundation/src/index.html")
+	// if err != nil {
+	// 	return
+	// }
+	indexTemplate := template.Must(template.New("index").Parse(string(resp)))
+	indexTemplate.Execute(os.Stdout, todos)
 }
