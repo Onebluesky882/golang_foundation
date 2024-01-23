ee46ab97dae115af83c3eaf9cd7994384fdfbdc8
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-01-23 16:46:23 +0700

    day10

--- src/main.go
+++ src/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+type Person struct {
+	name string
+	age  int
+}
+
+type customeSort struct {
+	Persons []*Person
+	less    func(i, j *Person) bool
+}
+
+func (p customeSort) Len() int {
+	return len(p.Persons)
+}
+
+func (p customeSort) Less(i, j int) bool {
+	return p.less(p.Persons[i], p.Persons[j])
+}
+
+func (p customeSort) Swap(i, j int) {
+	p.Persons[i], p.Persons[j] = p.Persons[j], p.Persons[i]
+}
 
 func main() {
-	fmt.Println("hello world")
+	p := []*Person{
+		{"a", 22}, {"b", 19}, {"c", 20}, {"d", 18}, {"e", 20}, {"aa", 20},
+	}
+	printPerson(p)
+	sort.Sort(customeSort{Persons: p, less: func(i, j *Person) bool {
+		if i.name != j.name {
+			return i.name < j.name
+		}
+		if i.age != j.age {
+			return i.age < j.age
+		}
+		return false
+	}})
+	fmt.Println("...........................")
+	printPerson(p)
+}
+
+func printPerson(p []*Person) {
+	for _, v := range p {
+		fmt.Println(*v)
+	}
 }
