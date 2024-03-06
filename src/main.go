2cd832b4ee7eeb32b43b65fa9c1f64a5341bf131
Onebluesky882 <119560471+Onebluesky882@users.noreply.github.com>
2024-03-06 12:57:27 +0700

    fucntion programing

--- src/main.go
+++ src/main.go
@@ -2,28 +2,15 @@ package main
 
 import "fmt"
 
-func createFib() func(int) []int {
-	fList := []int{0, 1, 1}
-	return func(n int) []int {
-		if n > len(fList) {
-			for n > len(fList) {
-				lastIndex := len(fList) - 1
-				fList = append(fList, fList[lastIndex]+fList[lastIndex-1])
-			}
-		}
-		return fList[:]
+func printEachLine(args ...interface{}) {
+	for _, v := range args {
+		fmt.Println(v)
 	}
-
 }
 
 func main() {
-
-	fib := createFib()
-
-	fmt.Println(fib(5)) // 0 1 1 2 3
-	fmt.Println(fib(6)) // 0 1 1 2 3 5
-	fmt.Println(fib(7)) // 0 1 1 2 3 5 8
-
-	fmt.Println(fib(16))
+	x := []interface{}{"asdfwf", "wfwfd", 233, "wfwf"}
+	printEachLine(x)
+	printEachLine(x...)
 
 }
